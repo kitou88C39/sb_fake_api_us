927187d4794c9612c5a2ed8cbe0960fa643732fc
kitou88C39 <kanfutrooper@icloud.com>
2023-11-20 13:07:57 +0900

    infrastructure/FakeApiClient.javaを編集

--- src/main/java/javanautas.fakeapius/infrastructure/FakeApiClient.java
+++ src/main/java/javanautas.fakeapius/infrastructure/FakeApiClient.java
@@ -1,5 +1,8 @@
 package javanautas.fakeapius.infrastructure;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(value = "fake-api", url = "${fake-api.url:#{null}}")
 public class FakeApiClient {
 
 }
