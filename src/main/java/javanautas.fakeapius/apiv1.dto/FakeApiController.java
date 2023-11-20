5a0119b39e53f954239f0055cbd3c275951a04d0
kitou88C39 <kanfutrooper@icloud.com>
2023-11-20 15:30:24 +0900

    appv1.dto/FakeApiController.java 編集

--- src/main/java/javanautas.fakeapius/apiv1.dto/FakeApiController.java
+++ src/main/java/javanautas.fakeapius/apiv1.dto/FakeApiController.java
@@ -1,5 +1,16 @@
 package javanautas.fakeapius.apiv1.dto;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javanautas.fakeapius.business.FakeApiService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/productos")
+@RequiredArgsConstructor
 public class FakeApiController {
+
+    private final FakeApiService service;
 
 }
