858cfa628b2329d2202a36ef69c1e953d6c0c41e
kitou88C39 <kanfutrooper@icloud.com>
2023-11-20 09:50:15 +0900

    first commit

---
+++ src/test/java/com/javnautas/fake_api_us/FakeApiUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javnautas.fake_api_us;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeApiUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
