4ce2e6f0dbdebbb270af1f511113176338dc0f67
kitou88C39 <kanfutrooper@icloud.com>
2023-11-20 11:30:40 +0900

    infrastructure/FakeApiUsApplication.javaを編集

---
+++ src/main/java/javanautas.fakeapius/FakeApiUsApplication.java
@@ -0,0 +1,11 @@
+package javanautas.fakeapius;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeApiUsApplication {
+    public static void main(String[] org) {
+        SpringApplication.run(FakeApiUsApplication.class, args);
+    }
+
+}
