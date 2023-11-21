a1160c49d2bfe331d2af957f216d581fe1730ec9
kitou88C39 <kanfutrooper@icloud.com>
2023-11-21 10:37:20 +0900

    client/ProductoRepository.java 編集

--- src/main/java/javanautas.fakeapius/infrastructure/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package javanautas.fakeapius.infrastructure;
-
-import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
-import org.springframework.stereotype.Repository;
-
-＠Repository
-public class ProductoRepository extends JpaProperties<ProductoEntity, String>{
-    
-}
