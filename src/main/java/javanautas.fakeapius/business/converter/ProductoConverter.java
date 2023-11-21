8b3acc87de57299f4a1b7e7dfefca0fd24038c78
kitou88C39 <kanfutrooper@icloud.com>
2023-11-21 15:05:17 +0900

    converter/ProductoConverter.java 編集

--- src/main/java/javanautas.fakeapius/business/converter/ProductoConverter.java
+++ src/main/java/javanautas.fakeapius/business/converter/ProductoConverter.java
@@ -1,19 +1,25 @@
 package javanautas.fakeapius.business.converter;
 
-import java.util.UUID;
-
-import org.springframework.stereotype.Component;
 
 import javanautas.fakeapius.apiv1.dto.ProductsDTO;
 import javanautas.fakeapius.infrastructure.entities.ProductoEntity;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Component
 public class ProductoConverter {
 
-    public ProductoEntity toEntity(ProductsDTO dtp){
+    public ProductoEntity toEntity(ProductsDTO dto) {
         return ProductoEntity.builder()
         .id(String.valueOf(UUID.randomUUID()))
-        .name(dto.getName())
-        
-
+        .nome(dto.getNome())
+        .categoria(dto.getCategpria())
+        .descricao(dto.getDescricao())
+        .preco(dto.getPreco())
+        .imagem(dto.getImagem())
+        .dataInclusao(dto.getDataInclusao())
+        .build(dto.getBuild())
+  }
 }
